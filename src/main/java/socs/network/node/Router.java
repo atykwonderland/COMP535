f8b1fe2055a8685261ae4614457a1f0e6dfab5cd
di-di-da <46284780+di-di-da@users.noreply.github.com>
2023-02-05 00:44:33 -0500

    ProcessStart in progress
    Can create a packet, initialize socket, send output and wait for response.

--- src/main/java/socs/network/node/Router.java
+++ src/main/java/socs/network/node/Router.java
@@ -1,9 +1,19 @@
 package socs.network.node;
 
+import socs.network.message.SOSPFPacket;
 import socs.network.util.Configuration;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Objects;
 
 
 public class Router {
@@ -64,9 +74,106 @@ public class Router {
   }
 
   /**
+   * create packet to be broadcast
+   */
+  private SOSPFPacket createPacket(Link link, short pType) {
+    SOSPFPacket p = new SOSPFPacket();
+    p.srcProcessIP = rd.processIPAddress;
+    p.srcProcessPort = rd.processPortNumber;
+    p.srcIP = rd.simulatedIPAddress;
+    p.dstIP = link.router2.simulatedIPAddress;
+    p.sospfType = pType;
+    p.routerID = rd.simulatedIPAddress;
+    p.neighborID = rd.simulatedIPAddress;
+
+    // public Vector<LSA> lsaArray = null; //
+    // TODO: how to populalte p.lsaArray
+
+    return p;
+  }
+
+  /**
    * broadcast Hello to neighbors
    */
   private void processStart() {
+    Socket client;
+    SOSPFPacket clientPacket;
+    SOSPFPacket serverPacket;
+    ObjectOutputStream outToServer;
+    ObjectInputStream inFromServer;
+    boolean isEmpty = true;
+
+    // Ensure there exists a connection for this router
+    for (Link p : ports) {
+      if (p != null) {
+        isEmpty = false;
+        break;
+      }
+    }
+    if (isEmpty) {
+      System.err.println("Warning: No routers connected to current router " + rd.simulatedIPAddress + ".");
+    } 
+
+    // Send HELLO to every connected router
+    for (int i=0; i<ports.length; i++) {
+      
+      // Check that the ports element is not empty, otherwise skip to next array element
+      if (ports[i] == null) {
+        continue;
+      }
+
+      String serverName = ports[i].router2.processIPAddress;
+      short port = ports[i].router2.processPortNumber;
+      RouterStatus R2Status = ports[i].router2.status;
+
+      // Check that the ports element is not empty, otherwise skip to next array element
+      if (ports[i] == null) {
+        continue;
+      }
+
+      // TODO add any other checks to the specific neighbor
+
+
+      
+      try {
+        // initialize the packet to broadcast to neighbors
+        clientPacket = createPacket(ports[i], (short) 0);
+        
+        // send packet to server and wait for response
+        client = new Socket(serverName, port);
+
+        outToServer = new ObjectOutputStream(client.getOutputStream());
+        outToServer.writeObject(clientPacket);
+
+        inFromServer = new ObjectInputStream(client.getInputStream());
+        serverPacket = (SOSPFPacket) inFromServer.readObject();
+
+        if (serverPacket != null) {
+          System.out.println("received HELLO from " + serverPacket.srcIP + ";");
+        }
+
+        // Check that response is a HELLO
+        if (serverPacket.sospfType == 0) {
+          // If HELLO received, set status of R2 as TWO_WAY
+          R2Status = RouterStatus.TWO_WAY;
+          System.out.println("set " + serverName + " STATE to TWO_WAY");
+        }
+
+      } catch (UnknownHostException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (ClassNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+
+      // initialize database sychronization process LSAUPDATE
+
+
+    }
 
   }
 
