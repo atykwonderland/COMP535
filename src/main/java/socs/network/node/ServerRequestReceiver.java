6cdeba80231ab925f3bbb29dfc3037519bdb4173
di-di-da <46284780+di-di-da@users.noreply.github.com>
2023-02-07 15:59:43 -0500

    start command completed on server side

--- src/main/java/socs/network/node/ServerRequestReceiver.java
+++ src/main/java/socs/network/node/ServerRequestReceiver.java
@@ -2,6 +2,7 @@ package socs.network.node;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import socs.network.message.SOSPFPacket;
@@ -20,6 +21,7 @@ public class ServerRequestReceiver implements Runnable {
         // Initialize output and input stream to receive HELLO
         try {
             ObjectInputStream inFromClient = new ObjectInputStream(lSocket.getInputStream());
+            ObjectOutputStream outToClient = new ObjectOutputStream(lSocket.getOutputStream());
             SOSPFPacket packetReceived;
 
             packetReceived = (SOSPFPacket) inFromClient.readObject();
@@ -29,10 +31,8 @@ public class ServerRequestReceiver implements Runnable {
 
                 // Change the status of the link to INIT
                 boolean isLinked = false;
+                Link link = null;
                 for (int i=0; i<4; i++) {
-                    Link link = null;
-                    // System.out.println(router.ports[i]);
-                    // System.out.println(router.ports[i].router2);
                     if (router.ports[i] == null) {
                         continue;
                     } else if (router.ports[i].router2.simulatedIPAddress.equals(packetReceived.srcIP)) {
@@ -41,15 +41,46 @@ public class ServerRequestReceiver implements Runnable {
                         isLinked = true;
                         System.out.println("set " + packetReceived.srcIP + " state to INIT;");
                         break;
-                    } else {
-                        System.out.println(router.ports[i].router2.simulatedIPAddress);
-                        System.out.println(packetReceived.srcIP);
                     }
                 }
                 if (!isLinked) {
+                    // If there are no links (i.e. ports array empty), return with error
                     System.err.println("packet received from " + packetReceived.srcIP + " is not linked to this router. No further actions.");
                     return;
                 }
+
+                // Otherwise, create HELLO packet to set for TWO_WAY
+                SOSPFPacket clientPacket = new SOSPFPacket(
+                    router.rd.processIPAddress, 
+                    router.rd.processPortNumber,
+                    router.rd.simulatedIPAddress,
+                    link.router2.simulatedIPAddress,
+                    (short) 0, 
+                    router.rd.simulatedIPAddress, 
+                    packetReceived.srcIP
+                );
+
+                // Send return HELLO packet to client
+                outToClient.writeObject(clientPacket);
+
+                // Wait for the second HELLO packet from client
+                packetReceived = (SOSPFPacket) inFromClient.readObject();
+                if (packetReceived == null) {
+                    System.err.println("Error: Packet is null.");
+                    inFromClient.close();
+                    outToClient.close();
+                    lSocket.close();
+                    return;
+                } else if (packetReceived.sospfType == 0) {
+                    // if HELLO packet received, set status to TWO_WAY
+                    System.out.println("received HELLO from " + packetReceived.srcIP + ";");
+                    link.router2.status = RouterStatus.TWO_WAY;
+                    System.out.println("set " + packetReceived.srcIP + " STATE set to TWO_WAY;");
+                }
+
+                inFromClient.close();
+                outToClient.close();
+                lSocket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
