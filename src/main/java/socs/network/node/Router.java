6bbe67ce2f579c91ea2740337a3fc1cb384b3b26
di-di-da <46284780+di-di-da@users.noreply.github.com>
2023-04-05 17:30:41 -0400

    Start router without attaching now available

--- src/main/java/socs/network/node/Router.java
+++ src/main/java/socs/network/node/Router.java
@@ -240,8 +240,7 @@ public class Router {
       }
     }
     if (isEmpty) {
-      System.err.println("Warning: No routers connected to current router " + rd.simulatedIPAddress + ".");
-      return;
+      System.err.println("Process has started but no links are connected to this router " + rd.simulatedIPAddress + ".");
     } 
 
     // Send HELLO to every connected router
